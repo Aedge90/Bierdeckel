6ce00f3a72dabf7cecff633330f9075c2f62abfb
Achim Herrmann <achhemann@gmail.com>
2016-01-31 23:50:16 +0100

    Arduino Bierdeggl Application
    working!!

---
+++ src/app/bierdeggl_arduino/hx711.cpp
@@ -0,0 +1,72 @@
+#include "hx711.h"
+#include <Arduino.h>
+static uint32_t _offset;
+static float _scale;
+
+void hx711_init(void)
+{
+	pinMode(SCLK, OUTPUT);
+	pinMode(SDI, INPUT);
+
+	digitalWrite(SCLK, HIGH);
+	delayMicroseconds(100);
+	digitalWrite(SCLK, LOW);
+
+	hx711_averageValue(32);
+	_offset = hx711_averageValue(32);
+	_scale = .5*742.0f;
+}
+
+uint32_t hx711_averageValue(uint8_t times)
+{
+	uint8_t i;
+	uint32_t sum = 0;
+	for (i = 0; i < times; i++)
+	{
+		sum += hx711_getValue();
+	}
+
+	return sum / times;
+}
+
+uint32_t hx711_getValue()
+{
+	uint8_t i = 0, j = 0;
+	//uint32_t data = 0;
+	uint8_t data[3] = {0,0,0};
+	while (digitalRead(SDI));
+	for (i = 24;i--;)
+	{
+		//	digitalWrite(SCLK, HIGH);
+		//	data |= (digitalRead(SDI))<<i;
+		//	digitalWrite(SCLK, LOW);
+	}
+	for (j = 3; j--;)
+	{
+		for (i = 8; i--;)
+		{
+			digitalWrite(SCLK, HIGH);
+			data[j] |= (digitalRead(SDI))<<i;
+			digitalWrite(SCLK, LOW);
+		}
+	}
+
+	digitalWrite(SCLK, HIGH);
+	digitalWrite(SCLK, LOW);
+
+	data[2] ^= 0x80;
+	
+	return ((uint32_t) data[2] << 16) | ((uint32_t) data[1] << 8) | (uint32_t) data[0];
+}
+
+float hx711_getGram(void)
+{
+	int32_t val = (hx711_averageValue(32) - _offset);
+	return ((float) val) / _scale;
+}
+
+int32_t hx711_get_mg(void)
+{
+	int32_t val = (hx711_averageValue(32) - _offset);
+	return 1000 * ( val / _scale);
+}
