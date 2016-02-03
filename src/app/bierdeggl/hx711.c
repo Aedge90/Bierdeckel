182aea0207c54112f906aafce32b8c097e89192d
Achim <achim.ad.daeubler@fau.de>
2016-02-03 18:14:07 +0100

    added some comments

--- src/app/bierdeggl/hx711.c
+++ src/app/bierdeggl/hx711.c
@@ -7,11 +7,13 @@ void hx711_init(void)
 	DDR_HX |= SCLK;
 	DDR_HX &= ~SDI;
 
+	//reset HX711
 	PORT_HX |= SCLK;
 	_delay_us(100);
 	PORT_HX &= ~SCLK;
 
 	hx711_averageValue(32);
+	//callibration. _offset is weight of empty glas
 	_offset = hx711_averageValue(32);
 	_scale = 371;//.5*742.0f;
 }
@@ -32,7 +34,9 @@ uint32_t hx711_getValue()
 {
 	uint8_t i = 0;
 	uint32_t data = 0;
+	//When output data isnt ready for retrieval digital output pin DOUT (=SDI) is high
 	while (PIN_HX & SDI);
+	//read data bit by bit by sending 24 positive clock pulses to PD_SCK pin
 	for (i = 24;i--;)
 	{
 		PORT_HX |= SCLK;
@@ -40,9 +44,12 @@ uint32_t hx711_getValue()
 		PORT_HX &= ~SCLK;
 	}
 
+	//25th pulse sets DOUT (=SDI) pin back to HIGH
 	PORT_HX |= SCLK;
 	PORT_HX &= ~SCLK;
 
+	//no other pulses are sent so we will be using input channel A with a gain of 128
+	
 	data ^= 0x800000;
 
 	return data;	
