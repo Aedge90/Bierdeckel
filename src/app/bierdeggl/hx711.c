b303576cb89d0a2d2c90c38d9618465166d3acdb
Achim <achim.ad.daeubler@fau.de>
2016-02-26 16:31:40 +0100

    changed names and corrected pin assignment of hx711

--- src/app/bierdeggl/hx711.c
+++ src/app/bierdeggl/hx711.c
@@ -4,13 +4,13 @@ static int32_t  _scale;
 
 void hx711_init(void)
 {
-	DDR_HX |= SCLK;
-	DDR_HX &= ~SDI;
+	DDR_HX |= SCK;
+	DDR_HX &= ~DT;
 
 	//reset HX711
-	PORT_HX |= SCLK;
+	PORT_HX |= SCK;
 	_delay_us(100);
-	PORT_HX &= ~SCLK;
+	PORT_HX &= ~SCK;
 
 	hx711_averageValue(32);
 	//callibration. _offset is weight of empty glas
@@ -34,19 +34,19 @@ uint32_t hx711_getValue()
 {
 	uint8_t i = 0;
 	uint32_t data = 0;
-	//When output data isnt ready for retrieval digital output pin DOUT (=SDI) is high
-	while (PIN_HX & SDI);
+	//When output data isnt ready for retrieval digital output pin DOUT (=DT) is high
+	while (PIN_HX & DT);
 	//read data bit by bit by sending 24 positive clock pulses to PD_SCK pin
 	for (i = 24;i--;)
 	{
-		PORT_HX |= SCLK;
-		data |= (uint32_t)((PINC & SDI)?1:0)<<i;
-		PORT_HX &= ~SCLK;
+		PORT_HX |= SCK;
+		data |= (uint32_t)((PINC & DT)?1:0)<<i;
+		PORT_HX &= ~SCK;
 	}
 
-	//25th pulse sets DOUT (=SDI) pin back to HIGH
-	PORT_HX |= SCLK;
-	PORT_HX &= ~SCLK;
+	//25th pulse sets DOUT (=DT) pin back to HIGH
+	PORT_HX |= SCK;
+	PORT_HX &= ~SCK;
 
 	//no other pulses are sent so we will be using input channel A with a gain of 128
 	
